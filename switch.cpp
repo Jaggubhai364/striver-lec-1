af44b94a34e78b55f9e304e12b989ae73215e5b3
bjkumar03 <bjkumar180@gmail.com>
2025-01-16 19:52:07 +0530

    Add switch.cpp to the repository

---
+++ switch.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+int x;
+cin >> x;
+switch (x)
+{
+case 1:
+    cout << "m";
+    break;
+case 2:
+    cout << "t";
+    break;
+default:
+    cout << "invalid";
+}
+}
