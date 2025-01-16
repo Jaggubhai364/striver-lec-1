46ffd25aace3a29dad3e802454369f90a650bbbc
bjkumar03 <bjkumar180@gmail.com>
2025-01-16 21:02:52 +0530

    Daily practice code commit on Thu 01/16/2025

--- passbyref.cpp
+++ passbyref.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 using namespace std;
 // only a copy of num is passed not the num adress where actually it is stored so in output it will output only 10 in main
-void dosome(int num){
-    cout << num <<endl;
-    num +=5;
-        cout << num <<endl;
+// void dosome(int num){
+//     cout << num <<endl;
+//     num +=5;
+//         cout << num <<endl;
 
-}
-int main(){
-    int num=10;
-    dosome(num);
-    cout<<num;
-}
+// }
+// int main(){
+//     int num=10;
+//     dosome(num);
+//     cout<<num;
+// }
 // void dosome(int &num){
 //     cout << num <<endl;
 //     num +=5;
@@ -22,3 +22,14 @@ int main(){
 //     dosome(num);
 //     cout<<num;
 // }
+void dosome(int arr[],int n){
+   arr[0]+=3;
+        cout << arr[0]<<endl;
+
+}
+int main(){
+    
+    int arr[5]={1,2,3,4,5};
+    dosome(arr,5);
+    cout<<arr[0];
+}
